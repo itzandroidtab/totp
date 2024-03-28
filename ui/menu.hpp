bccd80a1733bf9b87fbfa05f028e7d0320ccf06d
itzandroidtab <koen@itzandroidtab.nl>
2024-03-28 23:32:22 +0100

    reworked menu for better settings screen

--- ui/menu.hpp
+++ ui/menu.hpp
@@ -23,14 +23,29 @@ namespace menu {
         constexpr static uint32_t line_pixels = (visible_pixels + boarder_pixels);
 
         // calculate the amount of items that fit on a single screen
-        constexpr static uint32_t max_visible_lines = (Width / line_pixels);
+        constexpr static uint32_t max_visible_lines = (Height / line_pixels);
+
+        // check if we have any partial lines
+        constexpr static bool has_partial_lines = (max_visible_lines != ((Height + (line_pixels - 1)) / line_pixels));
 
         // pointer to the array that has all the labels
         const string *const data;
 
-        // previous amount of items shown on the display
-        uint32_t previous_shown_amount = 0;
+        // flag if the partial edge is at the top or bottom
+        bool partial_top_line = false;
 
+        // the previous indexes for the top and bottom
+        klib::vector2i previous_range = {0, max_visible_lines};
+
+        /**
+         * @brief Draw a rectangle at a specific position
+         * 
+         * @tparam FrameBuffer 
+         * @param fb 
+         * @param col 
+         * @param start_position 
+         * @param end_position 
+         */
         template <typename FrameBuffer>
         constexpr static void draw_rectangle(FrameBuffer& fb, const klib::graphics::color col, 
             const klib::vector2i start_position, const klib::vector2i end_position) 
@@ -51,25 +66,6 @@ namespace menu {
             }
         }
 
-        /**
-         * @brief Fill 
-         * 
-         * @tparam FrameBuffer 
-         * @param fb 
-         * @param index 
-         * @param col 
-         */
-        template <typename FrameBuffer>
-        constexpr static void fill_background(FrameBuffer& fb, const klib::vector2u& offset, 
-            const uint8_t index, const klib::graphics::color& col) 
-        {
-            // draw a rectangle
-            return draw_rectangle(fb, col,
-                klib::vector2i{3, static_cast<int32_t>(((index * line_pixels) + boarder_pixels))} - offset.cast<int32_t>(),
-                klib::vector2i{Width - 3, static_cast<int32_t>(((index * line_pixels) + (line_pixels)))} - offset.cast<int32_t>()
-            );
-        }
-
     public:
         menu(const string *const args):
             data(args)
@@ -79,90 +75,125 @@ namespace menu {
         void draw(FrameBuffer& fb, const klib::vector2u& offset, const uint32_t selected, const char** option,
             const klib::graphics::color foreground, const klib::graphics::color background, const bool* hidden)
         {
-            uint32_t onscreen_index = 0;
-            uint32_t hidden_by_scroll = 0;
+            // check if the selection is outside of the current range
+            if (selected < previous_range.x || selected >= previous_range.y) {
+                bool above;
 
-            // draw the menu and the option if it is provided
-            for (uint32_t i = 0; i < Count && (onscreen_index < max_visible_lines); i++) {
-                // check if the item should be skipped as we are out of the bounds 
-                // for scrolling
-                const bool scroll = int32_t((int32_t(selected) - i) - max_visible_lines) >= 0;
+                // move the range
+                if (selected < previous_range.x) {
+                    // mark we are above the previous top
+                    above = true;
 
-                // mark if we are hiding the value by scrolling
-                hidden_by_scroll += scroll;
+                    // set the selected as the minimum
+                    previous_range.x = selected;
+                }
+                else {
+                    // mark we are not above the previous top
+                    // but below the previous bottom
+                    above = false;
 
+                    // move the max past the selected
+                    previous_range.y = selected + 1;
+                }
+
+                // calculate the new bottom or top
+                uint32_t items = 0;
+
+                // calculate the other position
+                for (uint32_t i = 0; i < max_visible_lines; i++) {
+                    // get the index to check
+                    const int32_t index = above ? (selected + i) : (selected - i);
+
+                    // check if we are at 0 or at the max
+                    if (index < 0 || index >= Count) {
+                        break;
+                    }
+
+                    // check if the option is hidden
+                    if (hidden != nullptr && hidden[index]) {
+                        // skip the option if it is hidden
+                        continue;
+                    }
+
+                    // increment the before
+                    items++;
+                }
+
+                // check what to set
+                if (above) {
+                    previous_range.y = previous_range.x + items;
+                }
+                else {
+                    previous_range.x = previous_range.y - items;
+                }
+
+                // update if we have a partial top or bottom
+                partial_top_line = !above;
+            }
+
+            // the start offset
+            constexpr static int32_t start_offset = boarder_pixels + (boarder_pixels / 2);
+
+            // get the starting position
+            int32_t current_pixel_h = (partial_top_line ? 
+                (start_offset - (Height % line_pixels)) : start_offset
+            );
+
+            // get the top. If we have a partial line at the top we move the index. Same for
+            // the bottom. If we do not have a partial line at the top we have one at the bottom 
+            const uint32_t top = previous_range.x - (partial_top_line && has_partial_lines);
+            const uint32_t bottom = previous_range.y + ((!partial_top_line) && has_partial_lines);
+
+            // check if the top is still valid
+            for (uint32_t index = top; index < bottom; index++) {
                 // check if the option is hidden
-                if ((hidden != nullptr && hidden[i]) || scroll) {
+                if (hidden != nullptr && hidden[index]) {
                     // skip the option if it is hidden
                     continue;
                 }
 
-                // get the index we are drawing to the screen on
-                const uint32_t position_index = onscreen_index;
-
-                // get the local selection based on the amount of lines we have skipped
-                const uint32_t local_selection = selected - hidden_by_scroll;
-
                 // get the correct foreground and background to highglight a option
-                const klib::graphics::color col_foreground = ((local_selection == position_index) ? foreground : background);
-                const klib::graphics::color col_background = ((local_selection == position_index) ? background : foreground);
+                const klib::graphics::color col_foreground = ((index == selected) ? foreground : background);
+                const klib::graphics::color col_background = ((index == selected) ? background : foreground);
 
                 // fill the background
-                fill_background(fb, offset, position_index, col_background);
+                draw_rectangle(fb, col_background, 
+                    klib::vector2i{3, current_pixel_h - static_cast<int32_t>(boarder_pixels / 2)} - offset.cast<int32_t>(),
+                    klib::vector2i{Width - 3, current_pixel_h + static_cast<int32_t>(visible_pixels - (boarder_pixels / 2))} - offset.cast<int32_t>()
+                );
 
                 // draw the text on the screen
                 // write the text to the display
                 Font::template draw(
-                    fb, data[i], 
+                    fb, data[index], 
                     klib::vector2i{
-                        line_boarder_pixels, 
-                        static_cast<int32_t>(
-                            boarder_pixels + (boarder_pixels / 2) + 
-                            (position_index * line_pixels)
-                        )
+                        line_boarder_pixels, current_pixel_h
                     } - offset.cast<int32_t>(),
                     col_foreground, klib::graphics::transparent
                 );
-
-                // move the onscreen index
-                onscreen_index++;
 
                 // check if we need to show a option for the current item
-                if (option == nullptr || option[i] == nullptr) {
-                    // skip if we dont have one
-                    continue;
+                if (option != nullptr && option[index] != nullptr) {
+                    // get the length of the option string
+                    const uint32_t len = klib::string::strnlen(option[index], sizeof(string));
+
+                    // check if the string has any length
+                    if (len && (len != sizeof(string))) {
+                        // write the value to the display
+                        Font::template draw(
+                            fb, option[index], 
+                            klib::vector2i{
+                                static_cast<int32_t>(Width - (line_boarder_pixels + (Font::font::width * len))),
+                                current_pixel_h
+                            } - offset.cast<int32_t>(),
+                            col_foreground, klib::graphics::transparent
+                        );
+                    }
                 }
 
-                // get the length of the option string
-                const uint32_t len = klib::string::strnlen(option[i], sizeof(string));
-
-                // check if the string has any length
-                if (!len || (len == sizeof(string))) {
-                    continue;
-                }
-
-                // write the value to the display
-                Font::template draw(
-                    fb, option[i], 
-                    klib::vector2i{
-                        static_cast<int32_t>(Width - (line_boarder_pixels + (Font::font::width * len))),
-                        static_cast<int32_t>(
-                            boarder_pixels + (boarder_pixels / 2) + 
-                            (position_index * line_pixels)
-                        )
-                    } - offset.cast<int32_t>(),
-                    col_foreground, klib::graphics::transparent
-                );
+                // move the current pixel
+                current_pixel_h += line_pixels;
             }
-
-            // check if we need to clear parts of the screen
-            for (uint32_t i = onscreen_index; i < previous_shown_amount; i++) {
-                // clear any previous items from the screen
-                fill_background(fb, offset, i, klib::graphics::black);
-            }
-
-            // update the amount of items shown on the display
-            previous_shown_amount = onscreen_index;
         }
 
         template <typename FrameBuffer>
